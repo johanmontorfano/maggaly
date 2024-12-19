fc5771f24ea80e190450fab219370ac1299c58ba
Johan Montorfano <me@johanmontorfano.com>
2024-12-19 09:41:30 +0100

    fix: refactored

--- virtual_network/router.ts
+++ virtual_network/router.ts
@@ -7,7 +7,6 @@ import { Station } from "./network/components/station";
 import { Switches } from "./network/components/switches";
 import fs from "node:fs";
 
-let setupRoutes = 0;
 const app = express();
 export const context = new NetworkContext({
     depots: [
@@ -37,37 +36,24 @@ export const context = new NetworkContext({
 // The network will update 1 time per second. Nothing is made to support sub-sec
 // updates.
 setInterval(() => context.tick(), 1000);
-
-app.use(express.static("static"));
-app.get("/network/ui", require("./routes/app/viewer").default);
-
 // Every route in `routes/api` is imported, the path from `api` is the path
 // the route can be accessed from. Hence `routes/api/index.ts` is accessible
 // at `/api/`, or `routes/api/network/get.ts` becomes `/api/network/get`.
 console.log("[VirtualNetwork] Setting up routes...");
 fs.readdirSync("./routes/api", {recursive: true}).forEach((fp: any) => {
-    if (!fs.statSync("./routes/api/" + fp).isFile())
-        return;
+    if (!fs.statSync("./routes/api/" + fp).isFile()) return;
 
-    const api_route = "/api/" + (fp.endsWith("index.ts") ? 
-        fp.replace("index.ts", "") : fp.replace(".ts", ""));
+    const api_route = "/api/" + fp.replace(".ts", "").replace("index", "");
     const module = require("./routes/api/" + fp);
 
-    if (module.GET !== undefined) {
-        app.get(api_route, module.GET);
-        setupRoutes++;
-    }
-    if (module.POST !== undefined) {
-        app.post(api_route, module.POST);
-        setupRoutes++;
-    }
+    if (module.GET !== undefined) app.get(api_route, module.GET);
+    if (module.POST !== undefined) app.post(api_route, module.POST);
 });
-console.log(`[VirtualNetwork] ${setupRoutes} routes are available.`);
-
-app.listen(3000, () => console.log("[VirtualNetwork] Running on port 3000"));
-
+app.use(express.static("static"));
+app.get("/network/ui", require("./routes/app/viewer").default);
+app.listen(3000);
+console.log("[VirtualNetwork] Routes setup !");
 if (process.argv[2] === "--cli") {
-    console.log("CLI requested");
-
+    console.log("[CLI] CLI requested");
     require("./network/cli").initCli();
 }
